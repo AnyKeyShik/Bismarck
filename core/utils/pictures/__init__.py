e9c2b30a2cc6c12c8b4a16ab3f664a7b66ad9c21
AnyKeyShik <nikitav59@gmail.com>
2022-02-03 05:43:08 +0300

    [core][utils]: fix code style

--- core/utils/pictures/__init__.py
+++ core/utils/pictures/__init__.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
+#   Copyright (c) 2020.
+#
+#   Created by AnyKeyShik Rarity
+#
+#   Telegram: @AnyKeyShik
+#   GitHub: https://github.com/AnyKeyShik
+#   E-mail: nikitag594@gmail.com
+
 from .picture_grabber import PictureGrabber
--- core/utils/pictures/yandere_grabber.py
+++ core/utils/pictures/yandere_grabber.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 #   Copyright (c) 2020.
-#  #
+#
 #   Created by AnyKeyShik Rarity
-#  #
+#
 #   Telegram: @AnyKeyShik
 #   GitHub: https://github.com/AnyKeyShik
 #   E-mail: nikitag594@gmail.com
